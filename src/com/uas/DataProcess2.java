d33b953c5a88ecff6fb482169a1053e806de19b0
luthfibg <luthfinewzoneonline@gmail.com>
2022-08-16 17:46:27 +0700

    add Delete GUI Form

--- src/com/uas/DataProcess2.java
+++
@@ -1,8 +0,0 @@
-package com.uas;
-
-import javax.swing.*;
-
-public class DataProcess2 {
-    private JPanel panel1;
-    private JPanel mainPanel;
-}
---
+++ src/com/uas/DeleteData.java
@@ -0,0 +1,12 @@
+package com.uas;
+
+import javax.swing.*;
+
+public class DeleteData {
+    private JPanel deletePanel;
+    private JLabel jDeleteTitle;
+    private JTextField jTextDel;
+    private JButton jButtonCancelDel;
+    private JButton jButtonDelete;
+    private JLabel jLabelDeleteId;
+}
